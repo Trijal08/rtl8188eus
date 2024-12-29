7dc9541ce1af29e27922acf2bd3788b27b60aeea
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-12-28 20:06:02 -0500

    Fix all errors

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
+EXTRA_CFLAGS += -I$(srctree)/$(src)/include
+EXTRA_CFLAGS += -I$(srctree)/$(src)/hal/phydm
+EXTRA_CFLAGS += -I$(srctree)/$(src)/core/crypto
+EXTRA_CFLAGS += -I$(srctree)/$(src)/platform
+EXTRA_CFLAGS += -I$(srctree)/$(src)/os_dep
+EXTRA_CFLAGS += -I$(srctree)/$(src)/core
+
 EXTRA_CFLAGS += $(USER_EXTRA_CFLAGS) -fno-pie
 EXTRA_CFLAGS += -O1
 #EXTRA_CFLAGS += -O3
 EXTRA_CFLAGS += -Wno-unused-variable
-EXTRA_CFLAGS += -Wno-unused-value
+EXTRA_CFLAGS += -Wno-unused-value -Wno-incompatible-pointer-types
 EXTRA_CFLAGS += -Wno-unused-label
 EXTRA_CFLAGS += -Wno-unused-parameter
 EXTRA_CFLAGS += -Wno-unused-function
--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -418,7 +418,7 @@ u8 rtw_cfg80211_ch_switch_notify(_adapter *adapter, u8 ch, u8 bw, u8 offset, u8 
 		goto exit;
 		
 	#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0))
-	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef);
+	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef, 0, 0);
   #elif ((LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)) || (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 9, 0)))
 	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef, 0);
   #else
@@ -1104,12 +1104,11 @@ check_bss:
 		#endif
 
 		#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0) || defined(RHEL79))
-		#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 0, 0)
+		#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 78))
+		roam_info.links[0].bssid = cur_network->network.MacAddress;
+		#else
 		roam_info.channel = notify_channel;
 		roam_info.bssid = cur_network->network.MacAddress;
-		#else
-		roam_info.links[0].channel = notify_channel;
-		roam_info.links[0].bssid = cur_network->network.MacAddress;
 		#endif
 		roam_info.req_ie = pmlmepriv->assoc_req + sizeof(struct rtw_ieee80211_hdr_3addr) + 2;
 		roam_info.req_ie_len = pmlmepriv->assoc_req_len - sizeof(struct rtw_ieee80211_hdr_3addr) - 2;
@@ -9675,12 +9674,12 @@ void rtw_wdev_unregister(struct wireless_dev *wdev)
 	rtw_cfg80211_indicate_scan_done(adapter, _TRUE);
 
 	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 11, 0)) || defined(COMPAT_KERNEL_RELEASE)
-	#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0))
-	if (wdev->current_bss) {
-	#elif (LINUX_VERSION_CODE > KERNEL_VERSION(6, 0, 0))
+	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 2))
 	if (wdev->links[0].client.current_bss) {
-	#else
+	#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 78))
 	if (wdev->connected) {
+	#else
+	if (wdev->current_bss) {
 	#endif
 		RTW_INFO(FUNC_ADPT_FMT" clear current_bss by cfg80211_disconnected\n", FUNC_ADPT_ARG(adapter));
 		rtw_cfg80211_indicate_disconnect(adapter, 0, 1);
